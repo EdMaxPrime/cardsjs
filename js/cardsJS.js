b34a29e4ee3e8932531f8bcb0c82651bdc786c7d
EdMaxPrime <max.zlotskiy@gmail.com>
2014-08-27 15:04:11 -0400

    Added more operators and constants

--- js/cardsJS.js
+++ js/cardsJS.js
@@ -9,7 +9,62 @@ function CardScript(code) {
             union : '+',
             complement : '-',
             duplicates : '++',
-            antiDuplicate : '--'
+            antiDuplicate : '--',
+            add : '+',
+            minus : '-',
+            multiply : '*',
+            divide : '/',
+            assign : '=',
+            relative : '~',
+            globalStack : '$',
+            lengthOfThis : '.',
+            syncBoth : '<=>',
+            syncR2L : '=>',
+            syncL2R : '<=',
+            string : '"',
+            endFunction : '/',
+            returnOP : '>>',
+            funcStatementStart : '<',
+            funcStatementEnd : '>',
+            anonymous : '\\',
+            random : '?',
+            accessProperty : '(',
+            endPropertyAccess : ')',
+            startList : '[',
+            endList : ']',
+            listSeparator : ','
+        },
+        constants : {
+            bool : {
+                bool : 'BOOLEAN',
+                True : 'true',
+                False : 'false'
+            },
+            number : {
+                number : 'NUMBER',
+                fraction : 'FRACTION',
+                decimal : 'DECIMAL',
+                integer : 'INTEGER',
+                whole : 'WHOLE',
+                natural : 'NATURAL'
+            },
+            string : 'STRING',
+            array : 'ARRAY',
+            generic : 'GENERIC',
+            nil : 'NULL',
+            error : 'ERROR',
+            func : 'FUNCTION',
+            card : {
+                card : 'CARD',
+                suit : 'SUIT',
+                rank : 'RANK',
+                wild : 'WILDCARD'
+            }
+        },
+        variables : {
+            deck : '$',
+            ranks : 'Ranks',
+            suits : 'Suits'
         }
     };
 }
